f9456a1b491742757c341bf4001a1fc85f768491
Derek Widmer <dwidmer99@gmail.com>
2021-07-05 13:44:05 -0400

    Added console.log to view user variable for debugging

--- app.js
+++ app.js
@@ -128,6 +128,7 @@ app.use((req, res, next) => {
     res.locals.success = req.flash('success')
     res.locals.error = req.flash('error')
     res.locals.user = req.user
+    console.log(req.user)
     next()
 })
 
